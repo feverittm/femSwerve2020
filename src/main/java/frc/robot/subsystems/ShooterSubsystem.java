0bf4f16fc7c91874b7a4a0be52d2f16af0069b64
Floyd Moore <floyd.moore@hp.com>
2020-10-18 16:51:48 -0700

    adding Hopper and Shooter subsystems

---
+++ src/main/java/frc/robot/subsystems/ShooterSubsystem.java
@@ -0,0 +1,96 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2019 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot.subsystems;
+
+import com.revrobotics.CANEncoder;
+import com.revrobotics.CANPIDController;
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.ControlType;
+import com.revrobotics.CANSparkMax.IdleMode;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
+
+public class ShooterSubsystem extends SubsystemBase {
+
+  private CANSparkMax mMotor1;
+  private CANSparkMax mMotor2;
+  private CANPIDController mController;
+  private CANEncoder mEncoder;
+
+  /**
+   * Creates a new ShooterSubsystem.
+   */
+  public ShooterSubsystem() {
+    mMotor1 = new CANSparkMax(Constants.Ports.SHOOTER_MOTOR_1, MotorType.kBrushless);
+    mMotor2 = new CANSparkMax(Constants.Ports.SHOOTER_MOTOR_2, MotorType.kBrushless);
+
+    mMotor1.restoreFactoryDefaults();
+    mMotor2.restoreFactoryDefaults();
+
+    mMotor1.setIdleMode(IdleMode.kCoast);
+    mMotor2.setIdleMode(IdleMode.kCoast);
+
+    mMotor2.follow(mMotor1);
+
+    mEncoder = mMotor1.getEncoder();
+    mEncoder.setVelocityConversionFactor(Constants.Values.SHOOTER_GEARING);
+
+    mController = mMotor1.getPIDController();
+    mController.setP(Constants.Values.SHOOTER_VELOCITY_GAINS.kP);
+    mController.setI(Constants.Values.SHOOTER_VELOCITY_GAINS.kI);
+    mController.setD(Constants.Values.SHOOTER_VELOCITY_GAINS.kD);
+    mController.setFF(Constants.Values.SHOOTER_VELOCITY_GAINS.kF);
+  }
+
+  public void setRPM(double rpm) {
+    mController.setReference(rpm, ControlType.kVelocity);
+  }
+
+  public void setYeeterPercent(double perc) {
+    mMotor1.set(perc);
+  }
+
+  public void goodStop() {
+    mMotor1.set(0.0);
+  }
+
+  public void updateSmartDashboard() {
+    SmartDashboard.putNumber("Shooter/encoderspeed", getRPMs());
+    SmartDashboard.putNumber("Shooter/Ball Ejection Speed", getBallSpeed());
+  }
+
+  public double getRPMs() {
+    return mEncoder.getVelocity();
+  }
+
+  public double getNeededBallVelocity(double distance) {
+    return (1 / (((Math.tan(Constants.Values.SHOOTER_RELEASE_ANGLE * (Math.PI / 180))) / (-4.9 * distance))
+        + ((2.49 - Constants.Values.SHOOTER_RELEASE_HEIGHT) / (4.9 * distance * distance))))
+        * (1 / Math.cos(Constants.Values.SHOOTER_RELEASE_ANGLE * (Math.PI / 180)));
+  }
+
+  public double getBallSpeed() {
+    return (getRPMs() / 60) * (Constants.Values.SHOOTER_CIRCUMFERENCE_CM / 100);
+  }
+
+  @Override
+  public void periodic() {
+    updateSmartDashboard();
+  }
+
+  private static ShooterSubsystem instance;
+
+  public static ShooterSubsystem getInstance() {
+    if (instance == null)
+      instance = new ShooterSubsystem();
+    return instance;
+  }
+}
