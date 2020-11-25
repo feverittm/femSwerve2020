8b7b234a09e185cc01c738f1898e57912d334667
Floyd Moore <floyd.moore@gmail.com>
2020-11-24 23:02:22 -0800

    module should be complete/

--- src/main/java/frc/robot/drivers/SpartanSwerveModule.java
+++ src/main/java/frc/robot/drivers/SpartanSwerveModule.java
@@ -13,26 +13,25 @@ import com.ctre.phoenix.motorcontrol.StatorCurrentLimitConfiguration;
 import com.ctre.phoenix.motorcontrol.SupplyCurrentLimitConfiguration;
 import com.ctre.phoenix.motorcontrol.TalonFXFeedbackDevice;
 import com.ctre.phoenix.motorcontrol.can.TalonFX;
-import com.revrobotics.CANAnalog;
-import com.revrobotics.CANEncoder;
-import com.revrobotics.CANPIDController;
-import com.revrobotics.CANSparkMax;
-import com.revrobotics.CANAnalog.AnalogMode;
-import com.revrobotics.CANSparkMax.IdleMode;
+import com.ctre.phoenix.motorcontrol.can.TalonSRX;
 
 import org.frcteam2910.common.math.Vector2;
 
+import edu.wpi.first.wpilibj.AnalogInput;
 import edu.wpi.first.wpilibj.controller.PIDController;
+import edu.wpi.first.wpiutil.math.MathUtil;
 import frc.robot.Constants;
 
 import org.frcteam2910.common.drivers.SwerveModule;
 
 /**
- * Driver for Team 997's 2020 revision of the 2910 swerve module.  Leveraged from team 3663 code.
+ * Driver for Team 997's 2020 revision of the 2910 swerve module. Leveraged from
+ * team 3663 code.
  * <p>
- * This implementation assumes that two Spark MAXs are used to control each
- * module over CAN and that the angle Spark MAX is connected to an analog
- * encoder.
+ * This implementation assumes that the drive motor is a Falcon 500 (TalonFX)
+ * and a CIM/miniCIM connected to a TalonSRX moves the angle of the module. The
+ * angle of the modile is measured by an MA3 encoder connected to an analog
+ * input on the RoboRio.
  * <p>
  * The drive distance units default to inches. This can be changed using
  * {@link SpartanSwerveModule#setDriveTicksPerUnit(double)}
@@ -47,16 +46,11 @@ public final class SpartanSwerveModule extends SwerveModule {
 
     private final double offsetAngle;
 
-    private final CANSparkMax angleMotor;
+    private final TalonSRX angleMotor;
     private final TalonFX driveMotor;
+    private final AnalogInput angleEncoder;
 
-    private final CANAnalog angleAbsoluteEncoder;
-    private final CANEncoder angleMotorEncoder;
-    private final PIDController anglePIDController;
-
-    private final double ANGLE_P = .5;
-    private final double ANGLE_I = 0.0;
-    private final double ANGLE_D = 0.0;
+    private PIDController anglePIDController;
 
     /**
      * The amount of drive encoder ticks that occur for one unit of travel.
@@ -71,43 +65,37 @@ public final class SpartanSwerveModule extends SwerveModule {
      * @param angleMotor     the motor controller that controls the angle motor
      * @param driveMotor     the motor controller that controls the drive motor
      */
-    public SpartanSwerveModule(final Vector2 modulePosition, final double offsetAngle, final CANSparkMax angleMotor,
-            final TalonFX driveMotor) {
+    public SpartanSwerveModule(final Vector2 modulePosition, final double offsetAngle, final TalonSRX angleMotor,
+            final TalonFX driveMotor, AnalogInput angleEncoder) {
         super(modulePosition);
         this.offsetAngle = offsetAngle;
         this.angleMotor = angleMotor;
         this.driveMotor = driveMotor;
-
-        // Configure absolute encoder
-        this.angleAbsoluteEncoder = new CANAnalog(this.angleMotor, AnalogMode.kAbsolute);
-        this.angleAbsoluteEncoder.setInverted(false);
-
-        // Configure angle motor encoder
-        angleMotorEncoder = angleMotor.getEncoder();
-        angleMotorEncoder.setPositionConversionFactor(2 * Math.PI / ANGLE_TOTAL_GEAR_RATIO);
-        angleMotorEncoder.setPosition(readAngle());
-
-        // Configure PID controllers
-        anglePIDController = new PIDController(ANGLE_P, ANGLE_I, ANGLE_D);
-        anglePIDController.enableContinuousInput(0, 2 * Math.PI);
+        this.angleEncoder = angleEncoder;
 
         // Config angle motor
-        this.angleMotor.setIdleMode(IdleMode.kBrake);
+        angleMotor.configFactoryDefault();
+        angleMotor.setInverted(true);
+        angleMotor.configNeutralDeadband(0.001);
+
+        // Configure PID controllers
+        anglePIDController = new PIDController(Constants.Values.ANGLE_ROTATION_GAINS.kP, Constants.Values.ANGLE_ROTATION_GAINS.kI, Constants.Values.ANGLE_ROTATION_GAINS.kD);
+        anglePIDController.enableContinuousInput(0, 2 * Math.PI);
 
         // Configure drive motor
         driveMotor.configFactoryDefault();
         driveMotor.setInverted(false);
         driveMotor.configNeutralDeadband(0.001);
-        
+
         /* Config sensor used for Primary PID [Velocity] */
         driveMotor.configSelectedFeedbackSensor(TalonFXFeedbackDevice.IntegratedSensor, 0, Constants.Values.TIMEOUT_MS);
         driveMotor.setSelectedSensorPosition(0);
 
         /* Config the peak and nominal outputs */
-		driveMotor.configNominalOutputForward(0, Constants.Values.TIMEOUT_MS);
-	    driveMotor.configNominalOutputReverse(0, Constants.Values.TIMEOUT_MS);
-		driveMotor.configPeakOutputForward(1, Constants.Values.TIMEOUT_MS);
-		driveMotor.configPeakOutputReverse(-1, Constants.Values.TIMEOUT_MS);
+        driveMotor.configNominalOutputForward(0, Constants.Values.TIMEOUT_MS);
+        driveMotor.configNominalOutputReverse(0, Constants.Values.TIMEOUT_MS);
+        driveMotor.configPeakOutputForward(1, Constants.Values.TIMEOUT_MS);
+        driveMotor.configPeakOutputReverse(-1, Constants.Values.TIMEOUT_MS);
 
         driveMotor.config_kP(0, Constants.Values.DRIVE_VELOCITY_GAINS.kP, Constants.Values.TIMEOUT_MS);
         driveMotor.config_kI(0, Constants.Values.DRIVE_VELOCITY_GAINS.kI, Constants.Values.TIMEOUT_MS);
@@ -116,11 +104,12 @@ public final class SpartanSwerveModule extends SwerveModule {
 
         driveMotor.setNeutralMode(NeutralMode.Brake);
 
-        /* Setup current limiting
-		 *   enabled, Limit(amp), trigger Threshold(amp), Trigger Threshold Time(s)
+        /*
+         * Setup current limiting enabled, Limit(amp), trigger Threshold(amp), Trigger
+         * Threshold Time(s)
          */
-        driveMotor.configStatorCurrentLimit(new StatorCurrentLimitConfiguration(true, 40, 50, 2.0));
-        driveMotor.configSupplyCurrentLimit(new SupplyCurrentLimitConfiguration(true, 20, 25, 1.0));
+        driveMotor.configStatorCurrentLimit(new StatorCurrentLimitConfiguration(true, 20, 30, 1.0));
+        driveMotor.configSupplyCurrentLimit(new SupplyCurrentLimitConfiguration(true, 10, 15, 0.5));
     }
 
     /**
@@ -139,9 +128,13 @@ public final class SpartanSwerveModule extends SwerveModule {
         this.wheelRevolutionsPerUnit = driveTicksPerUnit;
     }
 
+    
+    /** 
+     * @return double
+     */
     @Override
     public double readAngle() {
-        double voltage = angleAbsoluteEncoder.getVoltage() - .03; // account for dead encoder spot
+        double voltage = angleEncoder.getVoltage() - .03; // account for dead encoder spot
         double angle = (voltage / 3.22) * 2.0 * Math.PI + offsetAngle;
         angle %= 2.0 * Math.PI;
         if (angle < 0.0) {
@@ -149,18 +142,6 @@ public final class SpartanSwerveModule extends SwerveModule {
         }
 
         return angle;
-    }
-
-    public double getEncoderVoltage() {
-        return angleAbsoluteEncoder.getPosition();
-    }
-
-    public double getIntegratedEncoderAngle() {
-        return Math.toDegrees(angleMotorEncoder.getPosition());
-    }
-
-    public void resetIntegratedEncoder() {
-        angleMotorEncoder.setPosition(readAngle());
     }
 
     /**
@@ -171,6 +152,10 @@ public final class SpartanSwerveModule extends SwerveModule {
         return driveMotor.getSelectedSensorVelocity() / 60.0 / DRIVE_GEAR_RATIO / wheelRevolutionsPerUnit;
     }
 
+    
+    /** 
+     * @return double
+     */
     @Override
     public double readDistance() {
         return driveMotor.getSelectedSensorPosition() / wheelRevolutionsPerUnit;
@@ -181,11 +166,16 @@ public final class SpartanSwerveModule extends SwerveModule {
      */
     @Override
     public void setTargetAngle(double angle) {
-        angleMotor.set(anglePIDController.calculate(readAngle(), angle));
+        double output_angle = MathUtil.clamp(anglePIDController.calculate(readAngle()), -0.5, 0.5);
+        angleMotor.set(ControlMode.Position, output_angle);
     }
 
+    
+    /** 
+     * @param output
+     */
     @Override
     public void setDriveOutput(double output) {
-        driveMotor.set(ControlMode.PercentOutput, output);
+        driveMotor.set(ControlMode.Velocity, output);
     }
 }
